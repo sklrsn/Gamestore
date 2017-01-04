5855473bed833b7e92e1604a97efdf1d591d5ddd
Kalaiarasan Saminathan <kalaiarasan.saminathan@aalto.fi>
2017-01-04 23:03:52 +0200

    URL changes for update profile

--- gamestore/gamestoreui/urls.py
+++ gamestore/gamestoreui/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.update_profile, name='update_profile'),
+    url(r'^update/$', views.update_profile, name='update_profile'),
 ]
