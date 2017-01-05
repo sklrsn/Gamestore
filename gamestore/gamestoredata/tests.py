2fd1e9738ad25bad39cc43e59601711ee9edf798
Kalaiarasan Saminathan <kalaiarasan.saminathan@aalto.fi>
2017-01-06 00:52:58 +0200

    Testcases

--- gamestore/gamestoredata/tests.py
+++ gamestore/gamestoredata/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, client
+from django.contrib.auth.models import User
+from .models import UserProfile
 
-# Create your tests here.
+
+class UserProfileTest(TestCase):
+    def setUp(self):
+        self.c = client.Client()
+
+    def test_profile_valid_url(self):
+        response = self.client.get('/profile/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_profile_invalid_url(self):
+        response = self.client.get('/profile/')
+        self.assertEqual(response.status_code, 404)
+
+        response = self.client.get('/profile/login')
+        self.assertEqual(response.status_code, 301)
+
+    def test_user_insert(self):
+        user = User(email="abc@abc.com", password="abcdefgh123", username="abc");
+        user.save();
+        user = User.objects.get(email="abc@abc.com")
+        self.assertEqual(user.username, 'abc')
--- gamestore/gamestoreui/tests.py
+++ gamestore/gamestoreui/tests.py
@@ -1,3 +1,44 @@
-from django.test import TestCase
+from django.test import TestCase, client
 
-# Create your tests here.
+
+class AppNavigationTest(TestCase):
+    def setUp(self):
+        self.client = client.Client()
+
+    def test_valid_login_url(self):
+        response = self.client.get('/profile/login/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_login_url(self):
+        response = self.client.get('/profile/login')
+        self.assertEqual(response.status_code, 301)
+
+    def test_valid_change_password_url(self):
+        response = self.client.get('/profile/change_password/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_change_password_url(self):
+        response = self.client.get('/profile/change_password')
+        self.assertEqual(response.status_code, 301)
+
+    def test_valid_update_profile_url(self):
+        response = self.client.get('/profile/update_profile/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_invalid_update_profile_url(self):
+        response = self.client.get('/profile/update_profile')
+        self.assertEqual(response.status_code, 301)
+
+    def test_valid_register_user_url(self):
+        response = self.client.get('/profile/register_user/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_invalid_register_user_url(self):
+        response = self.client.get('/profile/register_user')
+        self.assertEqual(response.status_code, 301)
+
+    def test_logout_url(self):
+        response = self.client.get('/profile/logout/')
+        self.assertEqual(response.status_code, 302)
+
+
