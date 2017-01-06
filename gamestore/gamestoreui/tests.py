f165c1063a18f15b711a044a3c0e427ee981743a
Kalaiarasan Saminathan <kalaiarasan.saminathan@aalto.fi>
2017-01-06 15:48:32 +0200

    View changes

--- gamestore/gamestoreui/tests.py
+++ gamestore/gamestoreui/tests.py
@@ -1,7 +1,11 @@
 from django.test import TestCase, client
+from django.contrib.auth.models import User
+from django.http import HttpRequest
+from .views import update_profile
+from .forms import UserProfileForm
 
 
-class AppNavigationTest(TestCase):
+class AuthenticationTest(TestCase):
     def setUp(self):
         self.client = client.Client()
 
@@ -14,6 +18,10 @@ class AppNavigationTest(TestCase):
         self.assertEqual(response.status_code, 301)
 
     def test_valid_change_password_url(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('12345678')
+        user.save()
+        self.client.login(username="testuser", password="12345678")
         response = self.client.get('/profile/change_password/')
         self.assertEqual(response.status_code, 200)
 
@@ -22,8 +30,13 @@ class AppNavigationTest(TestCase):
         self.assertEqual(response.status_code, 301)
 
     def test_valid_update_profile_url(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('12345678')
+        user.save()
+        logged_in = self.client.login(username="testuser", password="12345678")
+        self.assertEqual(logged_in, True)
         response = self.client.get('/profile/update_profile/')
-        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.status_code, 200)
 
     def test_invalid_update_profile_url(self):
         response = self.client.get('/profile/update_profile')
@@ -37,8 +50,12 @@ class AppNavigationTest(TestCase):
         response = self.client.get('/profile/register_user')
         self.assertEqual(response.status_code, 301)
 
-    def test_logout_url(self):
-        response = self.client.get('/profile/logout/')
-        self.assertEqual(response.status_code, 302)
+    def test_logout(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('12345678')
+        user.save()
+        logged_in = self.client.login(username="testuser", password="12345678")
+        self.assertEqual(logged_in, True)
+        self.client.get('/profile/logout/')
 
 
--- gamestore/gamestoreui/views.py
+++ gamestore/gamestoreui/views.py
@@ -12,84 +12,105 @@ from .forms import UserForm, UserProfileForm
 @login_required
 @transaction.atomic
 def update_profile(request):
-    if request.method == 'POST':
-        profile_form = UserProfileForm(request.POST, instance=request.user.userprofile)
-        if profile_form.is_valid():
-            profile_form.save()
-            messages.success(request, 'Your profile was successfully updated!')
-            return render(request, 'profiles/success.html')
+    try:
+        if request.method == 'POST':
+            profile_form = UserProfileForm(request.POST, instance=request.user.userprofile)
+            if profile_form.is_valid():
+                profile_form.save()
+                messages.success(request, 'Your profile was successfully updated!')
+                return render(request, 'profiles/success.html')
+            else:
+                messages.error(request, 'Please correct the error below.')
         else:
-            messages.error(request, 'Please correct the error below.')
-    else:
-        profile_form = UserProfileForm()
-    return render(request, 'profiles/update_profile.html', {
-        'profile_form': profile_form
-    })
+            profile_form = UserProfileForm()
+        return render(request, 'profiles/update_profile.html', {
+            'profile_form': profile_form
+        })
+    except Exception as e:
+        print(e)
+        return render(request, 'profiles/system_failure.html')
 
 
+@login_required
 def change_password(request):
-    if request.method == 'POST':
-        form = PasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)
-            messages.success(request, 'Your password was successfully updated!')
-            return render(request, 'profiles/success.html')
+    try:
+        if request.method == 'POST':
+            form = PasswordChangeForm(request.user, request.POST)
+            if form.is_valid():
+                user = form.save()
+                update_session_auth_hash(request, user)
+                messages.success(request, 'Your password was successfully updated!')
+                return render(request, 'profiles/success.html')
+            else:
+                messages.error(request, 'Please correct the error below.')
         else:
-            messages.error(request, 'Please correct the error below.')
-    else:
-        form = PasswordChangeForm(request.user)
-    return render(request, 'profiles/change_password.html', {
-        'form': form
-    })
+            form = PasswordChangeForm(request.user)
+        return render(request, 'profiles/change_password.html', {
+            'form': form
+        })
+    except Exception as e:
+        print(e)
+        return render(request, 'profiles/system_failure.html')
 
 
 def register_user(request):
-    context = RequestContext(request)
-    registered = False
-    if request.method == 'POST':
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
-        if user_form.is_valid() and profile_form.is_valid():
-            user = user_form.save()
-            user.set_password(user.password)
-            user.save()
-            profile = profile_form.save(commit=False)
-            profile.user = user
-            if 'picture' in request.FILES:
-                profile.picture = request.FILES['picture']
-            profile.save()
-            registered = True
+    try:
+        context = RequestContext(request)
+        registered = False
+        if request.method == 'POST':
+            user_form = UserForm(data=request.POST)
+            profile_form = UserProfileForm(data=request.POST)
+            if user_form.is_valid() and profile_form.is_valid():
+                user = user_form.save()
+                user.set_password(user.password)
+                user.save()
+                profile = profile_form.save(commit=False)
+                profile.user = user
+                if 'picture' in request.FILES:
+                    profile.picture = request.FILES['picture']
+                profile.save()
+                registered = True
+            else:
+                print(user_form.errors, profile_form.errors)
         else:
-            print(user_form.errors, profile_form.errors)
-    else:
-        user_form = UserForm()
-        profile_form = UserProfileForm()
-    return render(request, 'profiles/register.html', {
-        'user_form': user_form, 'profile_form': profile_form, 'registered': registered
-    }, context)
+            user_form = UserForm()
+            profile_form = UserProfileForm()
+        return render(request, 'profiles/register.html', {
+            'user_form': user_form, 'profile_form': profile_form, 'registered': registered
+        }, context)
+    except Exception as e:
+        print(e)
+        return render(request, 'profiles/system_failure.html')
 
 
 def user_login(request):
-    if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(username=username, password=password)
-        if user:
-            if user.is_active:
-                login(request, user)
-                return render(request, 'profiles/success.html')
+    try:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    login(request, user)
+                    return render(request, 'profiles/success.html')
+                else:
+                    return HttpResponse("Your Game store account is disabled.")
             else:
-                return HttpResponse("Your Game store account is disabled.")
+                print(
+                    "Invalid login details: {0}, {1}".format(username, password));
+                return HttpResponse("Invalid login details")
         else:
-            print(
-                "Invalid login details: {0}, {1}".format(username, password));
-            return HttpResponse("Invalid login details")
-    else:
-        return render(request, 'profiles/login.html')
+            return render(request, 'profiles/login.html')
+    except Exception as e:
+        print(e)
+        return render(request, 'profiles/system_failure.html')
 
 
 @login_required
 def user_logout(request):
-    logout(request)
-    return render(request, 'profiles/success.html')
+    try:
+        logout(request)
+        return render(request, 'profiles/success.html')
+    except Exception as e:
+        print(e)
+        return render(request, 'profiles/system_failure.html')
