2ef2b4157be4fb1a7d2e9baf79a721068d366798
Kalaiarasan Saminathan <kalaiarasan.saminathan@aalto.fi>
2017-01-19 01:47:47 +0200

    URL changes for index file

--- gamestore/gamestore/urls.py
+++ gamestore/gamestore/urls.py
@@ -6,5 +6,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^profile/', include('gamestoreui.urls')),
-    url(r'^$', include('gamestoreui.urls')),
+    url(r'^', include('gamestoreui.urls')),
 ]
