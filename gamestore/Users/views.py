bc377ec8878c6b3ff7c8f86ac38afdf952786dad
Sunil Kumar Mohanty <sunil.mohanty@aalto.fi>
2017-02-10 23:40:42 +0200

    player home page

--- gamestore/Users/views.py
+++ gamestore/Users/views.py
@@ -88,8 +88,10 @@ This view renders the dashboard depends on the user type
 def home(request):
     user = User.objects.get(username=request.user)
     current_user = UserProfile.objects.get(user=user)
-    games_list = Game.objects.filter(developer_info=user)
-
+    if(current_user.user_type=='D'):
+        games_list = Game.objects.filter(developer_info=user)
+    else :
+        games_list =Purchase.objects.filter(player_details=user)
     upload_form = GameUploadForm()
     return render(request, 'dashboard.html',
                   {'user_type': current_user.user_type, 'games_list': games_list, 'upload_form': upload_form})
